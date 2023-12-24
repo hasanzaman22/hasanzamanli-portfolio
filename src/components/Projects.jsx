30772833860e1574a7bf90ea00228399b8e13d72
Hasan Zamanli <hasanzaman022@gmail.com>
2023-12-24 18:07:31 +0400

    Add new projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,8 @@ import { ProjectCard } from "./ProjectCards";
 import projImg1 from "../assets/img/project-img1.png"
 import projImg2 from "../assets/img/project-img2.png"
 import projImg3 from "../assets/img/project-img3.png"
+import projImg4 from "../assets/img/project-img4.png"
+import projImg5 from "../assets/img/project-img5.png"
 import gradient2 from "../assets/img/gradientProjects2.png"
 
 export const Projects = () => {
@@ -15,17 +17,29 @@ export const Projects = () => {
             link: "https://github.com/hasanzaman22/IEAvirtualAssistant"
         },
         {
-            title: "FIRST Project",
-            description: "It is a simple and fully responsive website. This website include HTML, CSS, SCSS and JavaScript.",
-            imgUrl: projImg2,
-            link: "https://hasanzaman22.github.io/FIRST-Website-Project-Full-Responsive.github.io/"
+            title: "First Rest Api Project with Java",
+            description: "My first Rest Api project with Java, Spring Boot and MySql. In Northwind database.",
+            imgUrl: projImg4,
+            link: "https://github.com/hasanzaman22/RestApi-Java-Springboot-MySql"
         },
         {
             title: "React Chat App",
             description: "Svarka Chat App with React and Chat Engine.",
             imgUrl: projImg3,
             link: "https://github.com/hasanzaman22/React-Chat-App"
-        }
+        },
+        {
+            title: "Calculator with Java",
+            description: "Simple Calculator with Java.",
+            imgUrl: projImg5,
+            link: "https://github.com/hasanzaman22/Calculator-With-Java"
+        },
+        {
+            title: "FIRST Project",
+            description: "It is a simple and fully responsive website. This website include HTML, CSS, SCSS and JavaScript.",
+            imgUrl: projImg2,
+            link: "https://hasanzaman22.github.io/FIRST-Website-Project-Full-Responsive.github.io/"
+        },
     ]
 
     return(
